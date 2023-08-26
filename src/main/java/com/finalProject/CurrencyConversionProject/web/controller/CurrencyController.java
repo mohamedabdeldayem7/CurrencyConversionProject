e24199f60cefe5b6395e59735cac1324afdb2518
Mohamed Abdeldayem <142317163+mohamedabdeldayem7@users.noreply.github.com>
2023-08-26 19:42:00 +0300

    Update CurrencyController.java

--- src/main/java/com/finalProject/CurrencyConversionProject/web/controller/CurrencyController.java
+++ src/main/java/com/finalProject/CurrencyConversionProject/web/controller/CurrencyController.java
@@ -1,10 +1,7 @@
 package com.finalProject.CurrencyConversionProject.web.controller;
 
-<<<<<<< HEAD
 import com.finalProject.CurrencyConversionProject.CurrencyService.serviceImpl.CurrencyServiceImpl;
-=======
 import com.finalProject.CurrencyConversionProject.currencyService.serviceImpl.CurrencyServiceImpl;
->>>>>>> mohamed
 import com.finalProject.CurrencyConversionProject.dto.AmountConversionDto;
 import com.finalProject.CurrencyConversionProject.dto.FavoriteCurrenciesDto;
 import com.finalProject.CurrencyConversionProject.dto.TwoCurrenciesComparisonDto;
@@ -34,14 +31,7 @@ public class CurrencyController {
 
         return new ResponseEntity(model, HttpStatus.OK);
     }
-<<<<<<< HEAD
-    @PostMapping("/{base}")
-    public ResponseEntity<ResponseModel<?>> getConversionRates(@RequestBody List<String>currencies,
-                                                                @PathVariable("base") String base){
-        FavoriteCurrenciesDto response =  this.currencyService.compareCurrencies(currencies,base);
-        ResponseModel<Object> model = ResponseModel.<Object>builder()
-                .data(response).statusCode(HttpStatus.OK.value()).build();
-=======
+
     @PostMapping("/favorite-currencies")
     public ResponseEntity<ResponseModel<?>> getConversionRates(@RequestBody List<String>currencies,
                                                                @RequestParam(value = "base",required = false, defaultValue = "USD") String base){
@@ -50,10 +40,8 @@ public class CurrencyController {
                 .data(response)
                 .statusCode(HttpStatus.OK.value())
                 .status("success").build();
->>>>>>> mohamed
 
         return new ResponseEntity(model, HttpStatus.OK);
-
     }
     @GetMapping("/comparison")
     public ResponseEntity<ResponseModel<?>> compareTwoCurrencies(@RequestParam(value = "base",required = false, defaultValue = "USD") String base,
@@ -61,22 +49,12 @@ public class CurrencyController {
                                                                  @RequestParam(value = "target1",required = false, defaultValue = "USD") String target1,
                                                                  @RequestParam(value = "target2",required = false, defaultValue = "USD") String target2)  {
 
-<<<<<<< HEAD
-
-    @PostMapping("/{base}/{amount}")
-    public ResponseEntity<ResponseModel<?>> compareTwoCurrencies(@PathVariable("base") String base,
-                                                          @PathVariable("amount") Double amount,
-                                                          @RequestBody List<String> targetCurrecies)  {
-
-        TwoCurrenciesComparisonDto response =  this.currencyService.compareTwoCurrencies(base, amount, targetCurrecies);
-        ResponseModel<Object> model = ResponseModel.<Object>builder()
-=======
         TwoCurrenciesComparisonDto response =  this.currencyService.compareTwoCurrencies(base, amount, target1,target2);
         ResponseModel<TwoCurrenciesComparisonDto> model = ResponseModel.<TwoCurrenciesComparisonDto>builder()
->>>>>>> mohamed
                 .data(response).statusCode(HttpStatus.OK.value()).build();
         return new ResponseEntity(model, HttpStatus.OK);
     }
+    
     @GetMapping("/currencies")
     public ResponseEntity<ResponseModel<?>> getCurrencies(){
         List<Map<String, String>> currencies =  this.currencyService.getCurrencies();
