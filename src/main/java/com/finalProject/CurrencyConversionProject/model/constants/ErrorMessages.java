e6bed1d889aa27b773a0947223fb0ef769cdb86a
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2023-08-28 15:42:11 +0300

    adding modifying ErrorMessages

--- src/main/java/com/finalProject/CurrencyConversionProject/model/constants/ErrorMessages.java
+++ src/main/java/com/finalProject/CurrencyConversionProject/model/constants/ErrorMessages.java
@@ -4,5 +4,4 @@ public class ErrorMessages {
     public static String UNSUPPORTED_CURRENCY = "unsupported currency";
     public static String INVALID_AMOUNT = "invalid amount";
     public static String  ERROR_STATUS = "error";
-    public static String SUCCESS_STATUS = "success";
 }
